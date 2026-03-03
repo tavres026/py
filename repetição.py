2c0e08e9da458067b680e28a9f8fffe3e354b049
tavres026 <tmariaclara026@gmail.com>
2026-03-03 14:09:32 -0300

    Add files via upload

---
+++ repetição.py
@@ -0,0 +1,24 @@
+print("ola, mundo!")
+
+print("ola, mundo!")
+print("ola, mundo!")
+print("ola, mundo!")
+
+cont = 1 
+
+while cont <= 10: 
+    print("ola, mundo!")
+    cont += 1 # cont = cont + 1 
+print("FIM")    
+
+print(list(range(10)))
+print(list(range(1,10,2)))
+
+for cont in range(10):
+    print("ola, munddo!")
+print("FIM")     
+
+soma = 0
+for termo in range(1,11):
+    soma += termo
+print(soma)    
